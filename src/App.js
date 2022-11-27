c642e7d1ee8f2dc381c2b6d7c95782a62959439f
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-27 15:18:04 +0530

    filtering done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import './App.css';
-import {  Page } from "framer";
-
-import Home from "./Home/Home";
 import Slider from "./Slider/Slider";
 
 
 const App=()=>{
-  
-  
-  
-  
   return (
     <div>
       {/* <Home/> */}
       <Slider/>
-
-
-
       {/* <img src={pages[2].src}/>
     {pages.map((page,index)=>{
       console.log(page.src);
--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./Slider.css"
 import background from "../Images/4.png"
 import Controller from "../Images/3.png"
@@ -11,30 +11,30 @@ import Power1 from "../Images/12.png"
 import Headphone1 from "../Images/11.png"
 import Controller1 from "../Images/13.png"
 
-const colors = [Controller,blank, Power,blank,Headphone,blank,Controller,blank,Power,blank];
-const colors1 = [blank,Controller1,blank, Power1,blank, Headphone1,blank,Controller1,blank,Power1];
+const images = [Controller,blank, Power,blank,Headphone,blank,Controller,blank,Power,blank];
+const images1 = [blank,Controller1,blank, Power1,blank, Headphone1,blank,Controller1,blank,Power1];
 const delay = 2500;
 
 function Slider() {
-  const [index, setIndex] = React.useState(0);
-  // const [index1, setIndex1] = React.useState(0);
+  const [index, setIndex] = useState(0);
+  // const [index1, setIndex1] = useState(0);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setTimeout(
       () =>
         setIndex((prevIndex) =>
-          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+          prevIndex === images.length-1 ? 0 : prevIndex + 1
         ),
       2500
     );
-    return () => { };
+    
   }, [index]);
 
   // React.useEffect(() => {
   //   setTimeout(
   //     () =>
   //       setIndex1((prevIndex) =>
-  //         prevIndex === colors1.length - 1 ? 0 : prevIndex + 1
+  //         prevIndex === images1.length - 1 ? 0 : prevIndex + 1
   //       ),
   //     delay
   //   );
@@ -43,37 +43,34 @@ function Slider() {
 
   return (
     <div>
-      <img className='back' src={background} />
+        <img className='back' src={background} />
         <img className='left-mesh' src={mesh} />
         <img className='right-mesh' src={mesh} />
-      <div className="slideshow">
+        <div className="slideshow">
         
-        <div
-        className="slideshowSlider"
-        
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
-        {colors.map((backgroundColor, index) => (
-          <img
-            className="slide"
-            key={index}
-            src={backgroundColor}
-          />
-        ))}
-      </div>
-      </div>
+          <div
+          className="slideshowSlider"      
+          style={{ transform: `translate3d(${-index * 98}%, 0, 0)` }}
+          >
+          {images.map((image, index) => (
+            <img
+              className="slide"
+              key={index}
+              src={image}
+            />
+          ))}
+          </div>
+        </div>
 
       <div className='slideshow1'>
         <div
           className="slideshowSlider1"
-
-          style={{ transform: `translate3d(${-index * 34}%, 0, 0)` }}
-        >
-          {colors1.map((backgroundColor, index) => (
+          style={{ transform: `translate3d(${-index * 34}%, 0, 0)` }}>
+          {images1.map((image, index) => (
             <img
               className="slide1"
               key={index}
-              src={backgroundColor}
+              src={image}
             />
           ))}
         </div>
